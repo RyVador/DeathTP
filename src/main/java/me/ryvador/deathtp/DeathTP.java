0a2c8446a40deb58d84ea5af823a3943f2c55ab6
RyVador <70289057+RyVador@users.noreply.github.com>
2020-11-15 11:50:07 -0500

    Version 1.0.0

---
+++ src/main/java/me/ryvador/deathtp/DeathTP.java
@@ -0,0 +1,84 @@
+package me.ryvador.deathtp;
+
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class DeathTP extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("-------------------");
+        System.out.println("");
+        System.out.println("DeathTP Enabled!");
+        System.out.println("");
+        System.out.println("-------------------");
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("-------------------");
+        System.out.println("");
+        System.out.println("DeathTP Disabled!");
+        System.out.println("");
+        System.out.println("-------------------");
+
+    }
+
+    public Map<Player, Location> playersDeathLocation = new HashMap<>();
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e){
+        Player player = e.getEntity();
+        Location deathLocation = player.getLocation();
+        playersDeathLocation.put(player, deathLocation);
+        TextComponent message = new TextComponent("Click me to teleport back to your death location!");
+        message.setColor(ChatColor.GOLD);
+        message.setBold(true);
+        message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/deathtp"));
+        player.spigot().sendMessage(message);
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equals("deathtp")){
+
+            if (sender instanceof Player){
+
+                Player player = (Player) sender;
+                Location loc = playersDeathLocation.getOrDefault(player, null);
+                if (loc != null) player.teleport(loc);
+
+
+                return true;
+
+            } else {
+                System.out.println("Console cannot run this command!");
+            }
+
+        }
+
+
+
+
+
+        return true;
+    }
+}
